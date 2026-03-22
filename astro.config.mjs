fec6ee49969074458bff38702ad9772cddb3333f
mnkndn <manikndnpradeep@gmail.com>
2026-03-22 16:43:32 +0530

    added workflow file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://mnkndn.github.io",
+  base: "/lile",
   integrations: [tailwind()],
 });
